1313368b2cbb7d3994bd3beae925ce75594e55e6
Harshal Vadnere <vadnereh@seattleu.edu>
2019-01-15 11:50:25 -0800

    Add project files.

---
+++ App1/App1/Views/MainPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using App1.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : MasterDetailPage
+    {
+        Dictionary<int, NavigationPage> MenuPages = new Dictionary<int, NavigationPage>();
+        public MainPage()
+        {
+            InitializeComponent();
+
+            MasterBehavior = MasterBehavior.Popover;
+
+            //Updating default page to About page
+            //MenuPages.Add((int)MenuItemType.Browse, (NavigationPage)Detail);
+            MenuPages.Add((int)MenuItemType.About, (NavigationPage)Detail);
+        }
+
+        public async Task NavigateFromMenu(int id)
+        {
+            if (!MenuPages.ContainsKey(id))
+            {
+                switch (id)
+                {
+                    //fliping order to make default to About page
+                    case (int)MenuItemType.About:
+                        MenuPages.Add(id, new NavigationPage(new AboutPage()));
+                        break;
+
+                    case (int)MenuItemType.Browse:
+                        MenuPages.Add(id, new NavigationPage(new ItemsPage()));
+                        break;
+                    
+                }
+            }
+
+            var newPage = MenuPages[id];
+
+            if (newPage != null && Detail != newPage)
+            {
+                Detail = newPage;
+
+                if (Device.RuntimePlatform == Device.Android)
+                    await Task.Delay(100);
+
+                IsPresented = false;
+            }
+        }
+    }
+}
